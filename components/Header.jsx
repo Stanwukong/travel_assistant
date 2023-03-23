053e634503a58bbf2d41ea1ec415416f0c7d7080
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-03-23 13:13:20 +0100

    Fetching API

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,19 @@
-import { Flex, Input, InputGroup, InputRightElement, Menu, MenuButton } from "@chakra-ui/react"
+import {
+  Flex,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  theme,
+} from "@chakra-ui/react"
+import { Rating, ThemeProvider, Typography } from "@mui/material"
 import { Autocomplete } from "@react-google-maps/api"
 import React from "react"
-import { BiSearch, BiStar } from "react-icons/bi"
+import { BiChevronDown, BiHotel, BiMapAlt, BiRestaurant, BiSearch, BiStar } from "react-icons/bi"
 
 const Header = ({ setType, setRatings, setCoordinates }) => {
   return (
@@ -16,7 +28,7 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
     >
       <Flex>
         {/* <Autocomplete> */}
-        <InputGroup width={"35vw"} shadow="lg">
+        <InputGroup width={"35vw"} shadow="sm">
           <InputRightElement
             pointerEvents={"none"}
             children={<BiSearch color="gray" fontSize={20} />}
@@ -41,16 +53,144 @@ const Header = ({ setType, setRatings, setCoordinates }) => {
             px={4}
             py={2}
             bg={"white"}
-			rounded={'full'}
-			ml={4}
-			shadow="lg"
-			cursor={'pointer'}
+            rounded={"full"}
+            ml={4}
+            shadow="lg"
+            cursor={"pointer"}
+            _hover={{ bg: 'gray.200'}}
+            transition={'ease-in-out'}
+            transitionDuration={'0.3s'}
           >
-			<Menu>
-				<BiStar fontSize={25}/>
-				<MenuButton mx={2} transition='all 0.2s' borderRadius={'md'}>Choose Ratings</MenuButton>
-			</Menu>
-		  </Flex>
+            <Menu>
+              <BiStar fontSize={25} />
+              <MenuButton mx={2} transition="all 0.2s" borderRadius={"md"}>
+                Choose ratings
+              </MenuButton>
+              <MenuList>
+                <MenuItem
+                  display={"flex"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  onClick={() => setRatings("")}
+                >
+                  <Text fontSize={20} fontWeight={500} color={"gray.700"}>
+                    All Ratings
+                  </Text>
+                </MenuItem>
+                <MenuItem
+                  display={"flex"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  onClick={() => setRatings(2)}
+                >
+                  <Text fontSize={20} fontWeight={500} color={"orange.500"}>
+                    2.0
+                  </Text>
+                  {/* <Rating value={2} readOnly /> */}
+                </MenuItem>
+                <MenuItem
+                  display={"flex"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  onClick={() => setRatings(3)}
+                >
+                  <Text fontSize={20} fontWeight={500} color={"orange.500"}>
+                    3.0
+                  </Text>
+                  {/* <Rating value={3} readOnly /> */}
+                </MenuItem>
+                <MenuItem
+                  display={"flex"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  onClick={() => setRatings(4)}
+                >
+                  <Text fontSize={20} fontWeight={500} color={"orange.500"}>
+                    4.0
+                  </Text>
+                  {/* <Rating value={4} readOnly /> */}
+                </MenuItem>
+                <MenuItem
+                  display={"flex"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  onClick={() => setRatings(5)}
+                >
+                  <Text fontSize={20} fontWeight={500} color={"orange.500"}>
+                    4.5
+                  </Text>
+                  {/* <Rating value={5} readOnly /> */}
+                </MenuItem>
+              </MenuList>
+            </Menu>
+            <BiChevronDown fontSize={25} />
+          </Flex>
+
+          {/* Restaurants */}
+          <Flex
+            align={"center"}
+            justifyContent={"center"}
+            px={4}
+            py={2}
+            bg={"white"}
+            rounded={"full"}
+            ml={4}
+            shadow="lg"
+            cursor={"pointer"}
+            _hover={{ bg: 'gray.200'}}
+            transition={'ease-in-out'}
+            transitionDuration={'0.3s'}
+            onClick={() => setType('Restaurants')}
+          >
+            <BiRestaurant size={25}/>
+            <Text ml={3} fontSize={16} fontWeight={500}>
+              Restaurants
+            </Text>
+          </Flex>
+
+          {/* Hotels */}
+          <Flex
+            align={"center"}
+            justifyContent={"center"}
+            px={4}
+            py={2}
+            bg={"white"}
+            rounded={"full"}
+            ml={4}
+            shadow="lg"
+            cursor={"pointer"}
+            _hover={{ bg: 'gray.200'}}
+            transition={'ease-in-out'}
+            transitionDuration={'0.3s'}
+            onClick={() => setType('hotels')}
+          >
+            <BiHotel size={25}/>
+            <Text ml={3} fontSize={16} fontWeight={500}>
+              Hotels
+            </Text>
+          </Flex>
+
+          {/* Attractions */}
+          <Flex
+            align={"center"}
+            justifyContent={"center"}
+            px={4}
+            py={2}
+            bg={"white"}
+            rounded={"full"}
+            ml={4}
+            shadow="lg"
+            cursor={"pointer"}
+            _hover={{ bg: 'gray.200'}}
+            transition={'ease-in-out'}
+            transitionDuration={'0.3s'}
+            onClick={() => setType('attractions')}
+          >
+            <BiMapAlt size={25}/>
+            <Text ml={3} fontSize={16} fontWeight={500}>
+              Attractions
+            </Text>
+          </Flex>
         </Flex>
       </Flex>
     </Flex>
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,25 @@ import List from "../components/List"
 import Map from "../components/Map"
 import PlaceDetails from "../components/PlaceDetails"
 
+const places = [
+  { name: "Sample Place1" },
+  { name: "Sample Place2" },
+  { name: "Sample Place3" },
+  { name: "Sample Place4" },
+  { name: "Sample Place5" },
+  { name: "Sample Place6" },
+  { name: "Sample Place7" },
+  { name: "Sample Place8" },
+]
+
 function Home() {
   const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 })
 
-  const [type, setType] = useState("restutrants")
+  const [type, setType] = useState("restaurants")
 
-  const [ratings, setRatings] = useState("restutrants")
+  const [ratings, setRatings] = useState("")
+
+  const [isLoading, setIsLoading] = useState(false)
 
   return (
     <Flex
@@ -19,11 +32,14 @@ function Home() {
       width={"100vw"}
       height={"100vh"}
       maxHeight={"100vh"}
-      maxWidth={"100vh"}
       position={"relative"}
     >
-      <Header setType={setType} setRatings={setRatings} setCoordinates={setCoordinates}/>
-
+      <Header
+        setType={setType}
+        setRatings={setRatings}
+        setCoordinates={setCoordinates}
+      />
+      <List places={places} isLoading={isLoading} />
       <Map />
     </Flex>
   )
