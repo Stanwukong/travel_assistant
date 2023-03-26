9854b8d0f92072c2e1f8174de0548bb2ed856b33
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-03-26 11:06:21 +0100

    Filtering Places Data

--- components/Map.jsx
+++ components/Map.jsx
@@ -8,11 +8,15 @@ import {
 } from "@react-google-maps/api"
 import GoogleMapReact from "google-map-react"
 import styles from "./Map.module.css"
-import { Box } from "@chakra-ui/react"
+import { Box, Image } from "@chakra-ui/react"
 import { IoLocation } from "react-icons/io5"
+import { BiX } from "react-icons/bi"
 
 function Map({ coordinates, setCoordinates, setBounds, places }) {
-  const [map, setMap] = useState(null)
+
+
+  const [isCard, setIsCard] = useState(false);
+  const [cardData, setCardData] = useState(null);
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
@@ -38,7 +42,8 @@ function Map({ coordinates, setCoordinates, setBounds, places }) {
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
         }}
         onChildClick={() => {
-          console.log("What's good ma G")
+          setCardData(places[child])
+          setIsCard(true)
         }}
       >
         {places?.map((place, index) => (
@@ -46,6 +51,58 @@ function Map({ coordinates, setCoordinates, setBounds, places }) {
             <IoLocation color="red" fontSize={30}/>
           </Box>
         ))}
+
+        {isCard && (
+          <Box
+            width={'200px'}
+            height={'150px'}
+            bg={'whiteAlpha.900'}
+            position={'absolute'}
+            top={-12}
+            left={0}
+            shadow={'lg'}
+            rounded={'lg'}
+          >
+            <Image
+          objectFit={"cover"}
+          width={120}
+          height={120}
+          rounded={"lg"}
+          src={
+            cardData?.photo
+              ? cardData?.photo?.images?.large?.url
+              : "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60"
+          }
+        />
+         <Text
+              textTransform={"capitalize"}
+              width={40}
+              fontSize={"lg"}
+              fontWeight={500}
+              isTruncated
+            >
+              {cardData?.name}
+            </Text>
+            <Box
+              cursor={'pointer'}
+              position={'absolute'}
+              top={2}
+              right={2}
+              width={'full'}
+              height={'30px'}
+              bg={'red.300'}
+              rounded={'full'}
+              display={'flex'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              onClick={() => {
+                setIsCard(false)
+              }}
+            >
+              <BiX fontSize={20}/>
+            </Box>
+          </Box>
+        )}
       </GoogleMapReact>
     </Box>
   )
